3f18075852ee8279ba158b902ecbbd76b3c4916e
MarsalShyam <shyam12marsal@gmail.com>
2025-02-02 18:06:47 +0530

    donare,payment option and trust profile creating

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,10 +1,102 @@
 import React from 'react'
 
-const Username = ({params}) => {
+const Username = async ({ params }) => {
   return (
-    <div>
-      {params.user}
-    </div>
+    <>
+      <div className='cover w-full relative'>
+        <img className='object-cover w-full h-[350]' src="./banner.jpeg" alt="banner" />
+        <div className='absolute flex right-[48%] -bottom-14 border-2 rounded-full'>
+          <img width={100} height={100} className='rounded-full' src='./img2.webp' alt='img' />
+        </div>
+      </div>
+      <div className="info flex justify-center items-center my-20 flex-col gap-2">
+        <div className='font-bold text-lg'>
+          <h1>@{params.username}</h1>
+        </div>
+        <div className='text-slate-400'>9,719 memeber . 82 posts . $12,343/release</div>
+
+        <div className="payment flex gap-3 w-[80%] mt-11">
+          <div className="supporters w-1/2 bg-slate-900 rounded-lg p-10">
+            {/* show list of all the supporters as a leaderboarder  */}
+            <h2 className='text-2xl font-bold my-5'>Supporters</h2>
+            <ul className='mx-5'>
+              <li className='my-4 flex gap-2 items-center' >
+
+                <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                  <svg class="absolute w-10 h-1o text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                </div>
+
+                <span> Shubgam donated <span className='font-bold'>$30</span> with a message "I support You ❤️"</span>
+              </li>
+              <li className='my-4 flex gap-2 items-center' >
+
+                <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                  <svg class="absolute w-10 h-1o text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                </div>
+
+                <span> Shubgam donated <span className='font-bold'>$30</span> with a message "I support You ❤️"</span>
+              </li>
+              <li className='my-4 flex gap-2 items-center' >
+
+                <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                  <svg class="absolute w-10 h-1o text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                </div>
+
+                <span> Shubgam donated <span className='font-bold'>$30</span> with a message "I support You ❤️"</span>
+              </li>
+              <li className='my-4 flex gap-2 items-center' >
+
+                <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                  <svg class="absolute w-10 h-1o text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                </div>
+
+                <span> Shubgam donated <span className='font-bold'>$30</span> with a message "I support You ❤️"</span>
+              </li>
+              <li className='my-4 flex gap-2 items-center' >
+
+                <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                  <svg class="absolute w-10 h-1o text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                </div>
+
+                <span> Shubgam donated <span className='font-bold'>$30</span> with a message "I support You ❤️"</span>
+              </li>
+              <li className='my-4 flex gap-2 items-center' >
+
+                <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                  <svg class="absolute w-10 h-1o text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                </div>
+
+                <span> Shubgam donated <span className='font-bold'>$30</span> with a message "I support You ❤️"</span>
+              </li>
+            </ul>
+
+          </div>
+          <div className="makePayment w-1/2 bg-slate-900 rounded-lg p-10">
+            <h2 className='text-2xl font-bold my-5'>Make a Payment</h2>
+            <div className='flex gap-2 flex-col'>
+              {/* input for name and message */}
+              <div>
+                <input type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Name' />
+              </div>
+              <div>
+                <input type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Message' />
+              </div>
+              <div>
+                <input type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Amount' />
+              </div>
+              <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay</button>
+
+            </div>
+            {/* Or choose from these amount */}
+            <div className='flex gap-2 mt-2'>
+              <button className='bg-slate-800 p-3 rounded-lg'>Pay $10</button>
+              <button className='bg-slate-800 p-3 rounded-lg'>Pay $20</button>
+              <button className='bg-slate-800 p-3 rounded-lg'>Pay $50</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -87,7 +87,7 @@ const Navbar = () => {
           </Link>}
         {!session &&
           <Link href={"/login"}>
-            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >Login</button>
+            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >Join Us/Login</button>
           </Link>}
       </div>
     </nav>
